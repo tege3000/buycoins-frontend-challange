23460b89adc420e56278ac4a7dcf94467f941e50
Tito <teetoegeonu@gmail.com>
2020-11-27 21:04:17 +0100

    ADD - Project Setup Complete. Up Next, Implement coding challange

---
+++ app/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const SERVER_PORT = 3000;
+
+app.use(express.static('public'));
+
+app.listen(SERVER_PORT, () => {
+    console.log('Server listening on port ' + SERVER_PORT)
+});
--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,13 +1,14 @@
-const fetch = require("node-fetch");
+const baseUrl = "https://api.github.com/graphql";
+
 const github_data = {
     "token" : "78f0dc3ce341e01e770c15b44101c273467fb5d5",
     "username" : "tege3000"
 }
 
-const body = {
+const query_repositories = {
     "query" : `
         query { 
-            user(login: ${github_data["username"]}) {
+            user(login: "${github_data["username"]}") {
             repositories(first: 20) {
                 totalCount
                 nodes{
@@ -26,17 +27,15 @@ const body = {
     `
 };
 
-const baseUrl = "https://api.github.com/graphql";
-
 const headers =  {
     "Content-Type" : "application/json",
-    Authentication : "bearer " + github_data["token"]
+    Authorization : "bearer " + github_data["token"],
 }
 
 fetch(baseUrl, {
     method: "POST",
     headers: headers,
-    body: JSON.stringify(body)
+    body: JSON.stringify(query_repositories)
 })
     .then((response) => {        
         return response.json(); 
